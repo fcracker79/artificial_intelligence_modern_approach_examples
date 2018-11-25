c67f07e950004586000dee4e075f1996499cdb95
fcracker79 <fcracker79@gmail.com>
2018-11-25 17:56:03 +0100

    Iterative deepening search

--- ai/search_tree.py
+++ ai/search_tree.py
@@ -70,7 +70,8 @@ Solution = typing.NamedTuple(
     'Solution',
     (
         ('nodes', typing.List[Node]),
-        ('score', int)
+        ('score', int),
+        ('iterations', int)
     )
 )
 
@@ -80,26 +81,35 @@ QueuingFunction = typing.NewType(
 )
 
 
+ExpandFunction = typing.NewType('ExpandFunction', typing.Callable[[Node, 'SearchTree'], typing.Iterable[Node]])
+
+
 class SearchTree:
     def __init__(
             self, graph: Graph, root: str, goal: str,
+            expand_function: ExpandFunction,
             queuing_function: QueuingFunction):
         self.graph, self.root, self.goal = graph, root, goal
         self.queuing_function = queuing_function
         self.nodes = collections.deque()
         self.nodes.append(Node(root, None, 0, 0))
+        self._expand_function = expand_function
+        self.iterations = 0
 
     def log(self, *a, **kw):
         pass
         # print(*a, **kw)
 
     def get_children(self, node: Node) -> typing.Iterable[Node]:
+        children = self.graph.get_children(node.state)
+        self.iterations += len(children)
         return map(
             lambda d: Node(d, node, node.depth + 1, node.cost + self.graph.get_cost(node.state, d)),
-            self.graph.get_children(node.state)
+            children
         )
 
     def solve(self) -> typing.Optional[Solution]:
+        self.iterations = 0
         best_solution, best_score = None, sys.maxsize
         while self.nodes:
             node = self.nodes.pop()  # type: Node
@@ -117,7 +127,7 @@ class SearchTree:
                     best_solution, best_score = path + [node], node.cost
                     self.log(best_solution)
                 continue
-            children = self.get_children(node)
+            children = self._expand_function(node, self)
             parent_states = set(map(lambda d: d.state, path))
             self.log('children', node, children)
             children = list(filter(lambda d: d.state not in parent_states, children))
@@ -125,13 +135,29 @@ class SearchTree:
             self.nodes = self.queuing_function(self.nodes, children)
             node in self.nodes and self.nodes.remove(node)
 
-        return Solution(best_solution, best_score) if best_solution else None
+        return Solution(best_solution, best_score, self.iterations) if best_solution else None
 
     @classmethod
     def get_parents(cls, node: Node):
         while node.parent:
             node = node.parent
             yield node
+
+
+def _expand_children(node: Node, search_tree: SearchTree) -> typing.Iterable[Node]:
+    return search_tree.get_children(node)
+
+
+def _expand_at_level(level: int):
+    def _f(node: Node, search_tree: SearchTree) -> typing.Iterable[Node]:
+        nodes_to_expand = [node]
+        while nodes_to_expand:
+            node_to_expand = nodes_to_expand.pop()
+            if node_to_expand.depth == level:
+                yield node_to_expand
+            elif node_to_expand.depth < level:
+                nodes_to_expand.extend(_expand_children(node_to_expand, search_tree))
+    return _f
 
 
 def _breadth_first(queue: typing.Deque[Node], nodes: typing.Sequence[Node]) -> typing.Deque[Node]:
@@ -173,24 +199,47 @@ def _str_node(node: Node) -> str:
 def _print_solution(name: str, solution: typing.Optional[Solution]):
     _header(name)
     print('Score:', solution.score)
+    print('Iterations:', solution.iterations)
     print('Best path:', '\n\t'.join(map(_str_node, solution.nodes)))
 
 
 def _print_algo(name: str, graph: Graph, from_node: str, to_node: str, queuing_function: QueuingFunction):
     start = time.time()
-    solution = SearchTree(graph, from_node, to_node, queuing_function).solve()
+    solution = SearchTree(graph, from_node, to_node, _expand_children, queuing_function).solve()
     stop = time.time()
     _print_solution(name, solution)
     print('Elapsed time', stop - start)
 
 
+def _iterative_deepening_search(graph: Graph, from_node: str, to_node: str):
+    start = time.time()
+    best_solution = None  # type: Solution
+    iterations = 0
+    for max_depth in range(len(graph.nodes)):
+        if best_solution and len(best_solution.nodes) <= max_depth:
+            break
+        search_tree = SearchTree(graph, from_node, to_node, _expand_at_level(max_depth), _depth_first)
+        solution = search_tree.solve()
+        iterations += search_tree.iterations
+        if not solution:
+            continue
+        if not best_solution or solution.score < best_solution.score:
+            best_solution = solution
+    solution = Solution(nodes=best_solution.nodes, score=best_solution.score, iterations=iterations)
+    stop = time.time()
+    _print_solution('Iterative deepening search', solution)
+    print('Elapsed time', stop - start)
+
+
 def _run_algos(add_costs: bool):
+    from_node, to_node = 'Arad', 'Bucharest'
     _header('WITH COSTS' if add_costs else 'WITHOUT COSTS')
     g = _create_example_graph(add_costs=add_costs)
-    _print_algo('BREADTH FIRST', g, 'Arad', 'Bucharest', _breadth_first)
-    _print_algo('DEPTH FIRST', g, 'Arad', 'Bucharest', _depth_first)
-    _print_algo('UNIFORM COST', g, 'Arad', 'Bucharest', _uniform_cost)
-    _print_algo('DEPTH FIRST LIMIT 3', g, 'Arad', 'Bucharest', _depth_limited_first(3))
+    _print_algo('BREADTH FIRST', g, from_node, to_node, _breadth_first)
+    _print_algo('DEPTH FIRST', g, from_node, to_node, _depth_first)
+    _print_algo('UNIFORM COST', g, from_node, to_node, _uniform_cost)
+    _print_algo('DEPTH FIRST LIMIT 3', g, from_node, to_node, _depth_limited_first(3))
+    _iterative_deepening_search(g, from_node, to_node)
 
 
 def _run():
