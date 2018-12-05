07c9fa552eb69017c43b5c317c66a332a8b7dc09
fcracker79 <fcracker79@gmail.com>
2018-12-05 19:39:01 +0100

    Supporting limited arbitrary enqueue function.

--- ai/search_tree/eight_puzzle/entry_point.py
+++ ai/search_tree/eight_puzzle/entry_point.py
@@ -3,7 +3,8 @@ import typing
 from ai.search_tree.common_types import Node
 from ai.search_tree.eight_puzzle.puzzle import PuzzleGraph
 from ai.search_tree.print_tools import print_solution
-from ai.search_tree.queuing_functions import depth_first, uniform_cost, breadth_first, depth_limited_first
+from ai.search_tree.queuing_functions import depth_first, uniform_cost, breadth_first, depth_limited_first, \
+    limited_first
 from ai.search_tree.search_tree import SearchTree
 
 
@@ -18,7 +19,7 @@ def entry_point():
     search_tree = SearchTree(
         p, p.root,
         _expand_function,
-        depth_limited_first(30),
+        limited_first(20, depth_first),
         _conditional_function,
         solutions=1
     )
--- ai/search_tree/queuing_functions.py
+++ ai/search_tree/queuing_functions.py
@@ -2,7 +2,7 @@ import collections
 import itertools
 import typing
 
-from ai.search_tree.common_types import Node
+from ai.search_tree.common_types import Node, QueuingFunction
 
 
 def breadth_first(queue: typing.Deque[Node], nodes: typing.Sequence[Node]) -> typing.Deque[Node]:
@@ -21,9 +21,13 @@ def uniform_cost(queue: typing.Deque[Node], nodes: typing.Sequence[Node]) -> typ
     return collections.deque(sorted(itertools.chain(nodes, queue), reverse=True, key=lambda d: d.cost))
 
 
-def depth_limited_first(depth_limit: int):
+def limited_first(depth_limit: int, queuing_function: QueuingFunction):
     def _f(queue: typing.Deque[Node], nodes: typing.Sequence[Node]) -> typing.Deque[Node]:
         return collections.deque(
-            filter(lambda d: d.depth <= depth_limit, depth_first(queue, nodes))
+            filter(lambda d: d.depth <= depth_limit, queuing_function(queue, nodes))
         )
     return _f
+
+
+def depth_limited_first(depth_limit: int):
+    return limited_first(depth_limit, depth_first)
